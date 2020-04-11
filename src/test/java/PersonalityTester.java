9c0c4138369a1076f9c134a6792e40113546bc8f
DE0CH <chendeyao000@gmail.com>
2020-04-11 19:47:17 +0800

    finished writing tests

--- src/test/java/PersonalityTester.java
+++ src/test/java/PersonalityTester.java
@@ -1,7 +1,10 @@
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.NoSuchElementException;
 import java.util.Random;
+import java.util.Scanner;
 
 import static org.junit.Assert.*;
 
@@ -9,7 +12,28 @@ public class PersonalityTester
 {
     @Test
     public void helloTest(){
-        PersonalityTest p = new PersonalityTest();
-        assertEquals("Testing this class", p.toString());
+        try {
+            Scanner solution = new Scanner(new File("big_correct_output.txt"));
+            File in = new File("big_input.txt");
+            File out = new File("big_output.txt");
+            PersonalityTest.run(in, out);
+            Scanner answer = new Scanner(out);
+            while(solution.hasNextLine()) {
+                String s = solution.nextLine();
+                String a;
+                if (answer.hasNextLine()) {
+                    a = answer.nextLine().trim();
+                } else {
+                    break;
+                }
+                while (a.isEmpty() && answer.hasNextLine()) {
+                    a = answer.nextLine().trim();
+                }
+                assertEquals(s, a);
+            }
+            assertFalse(solution.hasNextLine());
+        } catch (IOException err) {
+            throw new AssertionError("Cannot open input files, or your program threw an IOException error.", err);
+        }
     }
 }
